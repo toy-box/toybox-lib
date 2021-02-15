7fe935264c83373d234d806ce146c9cee2c65abb
Huhui <wishgood@gmail.com>
2021-02-15 14:49:00 +0800

    fix

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,5 @@
 export default {
   esm: 'rollup',
   cjs: 'rollup',
-  extraExternals: ['prop-types', 'path-to-regexp', 'rc-picker'],
+  extraExternals: ['prop-types', 'path-to-regexp', 'rc-picker', 'rc-util'],
 };
