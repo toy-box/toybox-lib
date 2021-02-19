ba41cb9d48ea7e4867d471c72c7811d0b33629df
Huhui <wishgood@gmail.com>
2021-02-19 13:29:26 +0800

    垂直表格支持表头固定滚动时的样式

--- src/components/VerticalTable/index.md
+++ src/components/VerticalTable/index.md
@@ -103,30 +103,6 @@ const data = [
     billCycle: '2020-03-03',
     amount: 1500,
   },
-  {
-    id: '1236',
-    name: '实收',
-    billCycle: '2020-03-03',
-    amount: 1500,
-  },
-  {
-    id: '1236',
-    name: '实收',
-    billCycle: '2020-03-03',
-    amount: 1500,
-  },
-  {
-    id: '1236',
-    name: '实收',
-    billCycle: '2020-03-03',
-    amount: 1500,
-  },
-  {
-    id: '1236',
-    name: '实收',
-    billCycle: '2020-03-03',
-    amount: 1500,
-  },
 ];
 
 const columns = [
@@ -366,6 +342,98 @@ export default () => {
         columns={columns}
         rowSelection={rowSelection}
         rowKey={'id'}
+      />
+    </>
+  );
+};
+```
+
+### 横向滚动
+
+```tsx
+import React from 'react';
+import { VerticalTable } from '@toy-box/toybox-lib';
+
+const data = [
+  {
+    id: '1234',
+    name: '销售',
+    billCycle: '2020-01-01',
+    amount: 2000,
+  },
+  {
+    id: '1235',
+    name: '销售',
+    billCycle: '2020-02-01',
+    amount: 1300,
+  },
+  {
+    id: '1236',
+    name: '实收',
+    billCycle: '2020-03-03',
+    amount: 1500,
+  },
+  {
+    id: '1237',
+    name: '实收',
+    billCycle: '2020-03-03',
+    amount: 2500,
+  },
+  {
+    id: '1238',
+    name: '实收',
+    billCycle: '2020-05-03',
+    amount: 2500,
+  },
+  {
+    id: '1239',
+    name: '实收',
+    billCycle: '2020-06-03',
+    amount: 3500,
+  },
+  {
+    id: '1240',
+    name: '实收',
+    billCycle: '2020-07-03',
+    amount: 3700,
+  },
+  {
+    id: '1241',
+    name: '实收',
+    billCycle: '2020-08-03',
+    amount: 3900,
+  },
+  {
+    id: '1242',
+    name: '实收',
+    billCycle: '2020-09-03',
+    amount: 4000,
+  },
+];
+
+const columns = [
+  {
+    title: 'ID',
+    key: 'id',
+  },
+  {
+    title: '名称',
+    key: 'name',
+  },
+  {
+    title: '金额',
+    key: 'amount',
+  },
+];
+
+export default () => {
+  return (
+    <>
+      <VerticalTable
+        columnWidth={120}
+        headerWidth={100}
+        dataSource={data}
+        columns={columns}
       />
     </>
   );
--- src/components/VerticalTable/index.tsx
+++ src/components/VerticalTable/index.tsx
@@ -1,4 +1,10 @@
-import React, { ReactNode, useCallback, useMemo } from 'react';
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import {
   ColumnsType,
   DefaultRecordType,
@@ -16,12 +22,19 @@ import useSelection from 'antd/lib/table/hooks/useSelection';
 import useTitleColumns from 'antd/lib/table/hooks/useTitleColumns';
 import useLazyKVMap from 'antd/lib/table/hooks/useLazyKVMap';
 import defaultLocale from 'antd/lib/locale/en_US';
+import { useLayoutState, useTimeoutLock } from 'rc-table/lib/hooks/useFrame';
 import ColumnRow from './components/ColumnRow';
 import { TableContext } from './context/tableContext';
 
 import './style.less';
 
 type RowKeyFn<RecordType> = (record: RecordType) => string;
+
+// Used for conditions cache
+const EMPTY_DATA: any[] = [];
+
+// Used for customize scroll
+const EMPTY_SCROLL_TARGET = {};
 export interface VerticalTableProps<RecordType extends DefaultRecordType> {
   bordered?: boolean;
   columns: ColumnsType<DefaultRecordType>;
@@ -52,6 +65,8 @@ function VerticalTable<RecordType = DefaultRecordType>({
   getPopupContainer,
 }: VerticalTableProps<RecordType>) {
   const prefixCls = 'tbox-vertical-table';
+  const [pingedLeft, setPingedLeft] = useState(false);
+  const [pingedRight, setPingedRight] = useState(false);
   const {
     locale: contextLocale = defaultLocale,
     renderEmpty,
@@ -75,6 +90,8 @@ function VerticalTable<RecordType = DefaultRecordType>({
   const classes = classNames('tbox-vertical-table', {
     [`${prefixCls}-${size}`]: size,
     [`${prefixCls}-bordered`]: bordered,
+    [`${prefixCls}-ping-left`]: pingedLeft,
+    [`${prefixCls}-ping-right`]: pingedRight,
   });
 
   // ============================ RowKey ============================
@@ -89,13 +106,82 @@ function VerticalTable<RecordType = DefaultRecordType>({
   const childrenColumnName = 'selector';
 
   const [getRecordByKey] = useLazyKVMap(
-    dataSource || [],
+    dataSource || EMPTY_DATA,
     childrenColumnName,
     getRowKey,
   );
 
   const mergedData = useMemo(() => dataSource, [dataSource]);
   const pageData = useMemo(() => dataSource, [dataSource]);
+
+  // ============================ Scroll =============================
+  const scrollRef = React.useRef<any>();
+
+  const [setScrollTarget, getScrollTarget] = useTimeoutLock<any>(null);
+
+  function forceScroll(
+    scrollLeft: number,
+    target: HTMLDivElement | ((left: number) => void),
+  ) {
+    if (!target) {
+      return;
+    }
+    if (typeof target === 'function') {
+      target(scrollLeft);
+    } else if (target.scrollLeft !== scrollLeft) {
+      // eslint-disable-next-line no-param-reassign
+      target.scrollLeft = scrollLeft;
+    }
+  }
+  const onScroll = ({
+    currentTarget,
+    scrollLeft,
+  }: {
+    currentTarget: HTMLElement;
+    scrollLeft?: number;
+  }) => {
+    console.log('scrollLeft', scrollLeft);
+    const isRTL = direction === 'rtl';
+    const mergedScrollLeft =
+      typeof scrollLeft === 'number' ? scrollLeft : currentTarget.scrollLeft;
+
+    const compareTarget = currentTarget || EMPTY_SCROLL_TARGET;
+    if (!getScrollTarget() || getScrollTarget() === compareTarget) {
+      setScrollTarget(compareTarget);
+
+      forceScroll(mergedScrollLeft, scrollRef.current);
+    }
+
+    if (currentTarget) {
+      const { scrollWidth, clientWidth } = currentTarget;
+      if (isRTL) {
+        setPingedLeft(-mergedScrollLeft < scrollWidth - clientWidth);
+        setPingedRight(-mergedScrollLeft > 0);
+      } else {
+        setPingedLeft(mergedScrollLeft > 0);
+        setPingedRight(mergedScrollLeft < scrollWidth - clientWidth);
+      }
+    }
+  };
+
+  useEffect(() => {
+    function onWheel(e: WheelEvent) {
+      const { currentTarget, deltaX } = (e as unknown) as React.WheelEvent<
+        HTMLDivElement
+      >;
+      if (deltaX) {
+        onScroll({
+          currentTarget,
+          scrollLeft: currentTarget.scrollLeft + deltaX,
+        });
+      }
+    }
+    scrollRef.current?.addEventListener('wheel', onWheel);
+
+    return () => {
+      scrollRef.current?.removeEventListener('wheel', onWheel);
+    };
+  }, []);
 
   // ========================== Selections ==========================
   const [transformSelectionColumns, selectedKeySet] = useSelection<RecordType>(
@@ -134,7 +220,7 @@ function VerticalTable<RecordType = DefaultRecordType>({
   return (
     <TableContext.Provider value={tableContextValue}>
       <div className={classes}>
-        <table>
+        <table ref={scrollRef} onScroll={onScroll}>
           <tbody>
             {newColumns.map((column, index) => (
               <ColumnRow column={column} records={dataSource} key={index} />
