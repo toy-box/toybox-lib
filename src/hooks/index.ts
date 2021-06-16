7ab74df9f3dabf7288c7efc6c6818d9fc04751ef
Huhui <wishgood@gmail.com>
2021-06-16 11:15:49 +0800

    移除废弃代码

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,5 @@
 export { default as useBusinessObjectMeta } from './useBusinessObjectMeta';
 export { default as useFetchData } from './useFetchData';
-export { default as useFormModal } from './useFormModal';
-export { default as useFormDrawer } from './useFormDrawer';
 export { default as useScollSize } from './useScollSize';
 export { default as useQuery } from './useQuery';
 export { default as useQueryFilter } from './useQueryFilter';
